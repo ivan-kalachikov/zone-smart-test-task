3d61e4bf1c332ef87c89a892dcb5a7824f427104
Ivan Kalachikov <ivan.kalachikov@gmail.com>
2024-03-30 21:20:17 +0800

    tune linter and prettier + autofix

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,29 +1,31 @@
 module.exports = {
-  root: true,
-  env: {
-    node: true,
-  },
-  extends: [
-    "plugin:vue/vue3-essential",
-    "eslint:recommended",
-    "plugin:prettier/recommended",
-  ],
-  parserOptions: {
-    parser: "@babel/eslint-parser",
-  },
-  rules: {
-    "no-console": process.env.NODE_ENV === "production" ? "warn" : "off",
-    "no-debugger": process.env.NODE_ENV === "production" ? "warn" : "off",
-  },
-  overrides: [
-    {
-      files: [
-        "**/__tests__/*.{j,t}s?(x)",
-        "**/tests/unit/**/*.spec.{j,t}s?(x)",
-      ],
-      env: {
-        jest: true,
-      },
+    root: true,
+    env: {
+        node: true,
     },
-  ],
-};
+    extends: [
+        "plugin:vue/vue3-essential",
+        "eslint:recommended",
+        "plugin:prettier/recommended",
+    ],
+    parserOptions: {
+        parser: "@babel/eslint-parser",
+    },
+    rules: {
+        "no-console": process.env.NODE_ENV === "production" ? "warn" : "off",
+        "no-debugger": process.env.NODE_ENV === "production" ? "warn" : "off",
+        semi: ["error", "never"],
+        indent: ["error", 4],
+    },
+    overrides: [
+        {
+            files: [
+                "**/__tests__/*.{j,t}s?(x)",
+                "**/tests/unit/**/*.spec.{j,t}s?(x)",
+            ],
+            env: {
+                jest: true,
+            },
+        },
+    ],
+}

12ba23807a8706a0b1cd586781523c69df8fcdf2
Ivan Kalachikov <ivan.kalachikov@gmail.com>
2024-03-31 12:43:14 +0800

    fix env variable name

--- src/api/client.js
+++ src/api/client.js
@@ -1,7 +1,12 @@
 import axios from "axios"
 
+console.log(
+    "process.env.VUE_APP_API_BASE_URL",
+    process.env.VUE_APP_API_BASE_URL
+)
+
 const client = axios.create({
-    baseURL: process.env.BASE_URL,
+    baseURL: process.env.VUE_APP_API_BASE_URL,
     headers: {
         Accept: "application/json",
     },

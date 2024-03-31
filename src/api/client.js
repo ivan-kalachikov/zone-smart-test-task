39393d9ec899676d948c85695e477a24dc1ceea3
Ivan Kalachikov <ivan.kalachikov@gmail.com>
2024-03-31 22:14:53 +0800

    remove console.log

--- src/api/client.js
+++ src/api/client.js
@@ -1,10 +1,5 @@
 import axios from "axios"
 
-console.log(
-    "process.env.VUE_APP_API_BASE_URL",
-    process.env.VUE_APP_API_BASE_URL
-)
-
 const client = axios.create({
     baseURL: process.env.VUE_APP_API_BASE_URL,
     headers: {

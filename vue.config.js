36844ea9d64ac2624d0182c2cccdf34cbf964f00
Ivan Kalachikov <ivan.kalachikov@gmail.com>
2024-03-31 11:07:54 +0800

    add svg support

--- vue.config.js
+++ vue.config.js
@@ -2,8 +2,10 @@ const { defineConfig } = require("@vue/cli-service")
 module.exports = defineConfig({
     transpileDependencies: true,
     chainWebpack: (config) => {
-        config.module
-            .rule("pug")
+        const pugRule = config.module.rule("pug")
+        const svgRule = config.module.rule("svg")
+
+        pugRule
             .test(/\.pug$/)
             .use("pug-plain-loader")
             .loader("pug-plain-loader")
@@ -11,5 +13,15 @@ module.exports = defineConfig({
             .use("vue-pug-loader")
             .loader("vue-pug-loader")
             .end()
+
+        svgRule.uses.clear()
+        svgRule.delete("type")
+        svgRule.delete("generator")
+        svgRule
+            .use("vue-loader")
+            .loader("vue-loader")
+            .end()
+            .use("vue-svg-loader")
+            .loader("vue-svg-loader")
     },
 })

b1e785f071b8e2b83f12ee1aeb36862b87c9ed1f
Ivan Kalachikov <ivan.kalachikov@gmail.com>
2024-04-02 00:28:42 +0800

    add interceptor to refresh expired token

--- src/api/client.js
+++ src/api/client.js
@@ -1,9 +1,22 @@
 import axios from "axios"
+import store from "@/store"
 
 const client = axios.create({
     headers: {
         Accept: "application/json",
     },
 })
+
+client.interceptors.response.use(
+    (response) => response,
+    async (error) => {
+        console.log("error in interceptor", error)
+        if (error.response?.status === 401) {
+            await store.dispatch("refresh")
+            return axios.request(error.config)
+        }
+        return Promise.reject(error)
+    }
+)
 
 export default client
